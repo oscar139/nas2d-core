f068c98cb582bfba347bb3a9ed012a87e9215297
Alex <alexander.l.hunt951@gmail.com>
2024-03-25 15:51:05 -0400

    Initial implementation of ToolTip

--- Rectangle_ToolTipPrototype/ToolTip.cpp
+++ Rectangle_ToolTipPrototype/ToolTip.cpp
@@ -1,1 +1,13 @@
 #include "ToolTip.h"
+
+ToolTip::ToolTip() :mBox{0, 0, 0, 0}, mTitle{"Default Title"}, mFont{"../../data/fonts/opensans.ttf", 16}
+{
+	mDescription.push_back("Default Text Line 1");
+	mDescription.push_back("Default Text Line 2");
+}
+
+void ToolTip::draw()
+{
+	auto& r = NAS2D::Utility<NAS2D::Renderer>::get();
+	r.drawText(mFont, mTitle, NAS2D::Point{mBox.position.x + 5, mBox.position.y + 5}, NAS2D::Color::Red);
+}
--- Rectangle_ToolTipPrototype/ToolTip.h
+++ Rectangle_ToolTipPrototype/ToolTip.h
@@ -1,4 +1,23 @@
 #pragma once
+
+#include "NAS2D/Renderer/Renderer.h"
+#include "NAS2D/EventHandler.h"
+#include "NAS2D/Utility.h"
+#include "NAS2D/Math/Rectangle.h"
+#include <NAS2D/Resource/Font.h>
+
+#include <string.h>
+#include <vector>
+
 class ToolTip
 {
+	public:
+		ToolTip();
+		void draw();
+
+	private:
+		std::string mTitle;
+		std::vector<std::string> mDescription;
+		NAS2D::Rectangle<int> mBox;
+		NAS2D::Font mFont;
 };
